856a0d2e8f07e0d1e31ae553383b81d354edc2dc
pyneng <pyneng.course@gmail.com>
2022-09-08 17:34:20 +0000

    добавлены задания

---
+++ exercises/05_basic_scripts/task_5_5a.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Задание 5.3a
+
+Дополнить скрипт из задания 5.3 таким образом, чтобы, в зависимости
+от выбранного режима, задавались разные вопросы в запросе о номере
+VLANа или списка VLANов:
+* для access: 'Введите номер VLAN:'
+* для trunk: 'Введите разрешенные VLANы:'
+
+Плюсом будет решить задание без использования условия if и цикла for,
+но первый вариант решения лучше сделать так, как будет получаться.
+"""
+
+access_template = """switchport mode access
+switchport access vlan {}
+switchport nonegotiate
+spanning-tree portfast
+spanning-tree bpduguard enable
+"""
+
+trunk_template = """switchport trunk encapsulation dot1q
+switchport mode trunk
+switchport trunk allowed vlan {}
+"""
